f76dc3c549d0848c5e45d21b9df2c6e0a8f9f30d
Alan Xie <alanxie29@gmail.com>
2019-04-03 11:33:01 -0400

    subscription service and classes/interfaces

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
+import { Subscription, IPriceSubscription, IDurrationSubscription, Subscription2}  from '../../../models/subscription';
+import { identifierModuleUrl } from '@angular/compiler';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -20,10 +23,21 @@ export class LoginPage implements OnInit {
       password: ['', [Validators.required, Validators.minLength(6)]],
 
     });
+    this.hello()
   }
 
   onSubmit() {
     this.authService.login(this.loginForm.value).subscribe();
+  }
+
+  subscriptions: Subscription2[] = [new Subscription2()]
+
+  hello() {
+    let prices: IPriceSubscription[] = this.subscriptions
+    console.log(prices[0].DEC_ID)
+
+    let durrations: IDurrationSubscription[] = this.subscriptions
+    console.log(durrations[0].durration.day)
   }
 
 }
---
+++ src/models/subscription.ts
@@ -0,0 +1,67 @@
+import { identifierModuleUrl } from '@angular/compiler';
+
+export class Subscription implements IPriceSubscription, IDurrationSubscription {
+  _id: string
+  _name: string
+  _cost: number
+  _duration: number
+
+  constructor() {
+    this._id = 'asdfasfddadfasdf';
+    this._name = '';
+    this._cost = 0;
+    this._duration = 7777777;
+  }
+
+  get DEC_ID(): string {
+    return this.id
+  }
+
+  get id(): string {
+    return this._id
+  }
+
+  get name(): string {
+    return this._name
+  }
+  
+  get durration(): Date {
+    return new Date()
+  }
+
+  set name(value) {
+    value = "{" + value + "}"
+    this._name = value
+  }
+  get price(): number {
+    return this._cost
+  }
+}
+
+
+
+
+
+export class Subscription2 extends Subscription {
+
+}
+
+
+
+
+
+export interface IPriceSubscription {
+  DEC_ID: string;
+  name: string;
+  price: number;
+}
+
+export interface IDurrationSubscription {
+  id: string;
+  name: string;
+  durration: Date;
+}
+
+class Date {
+  day: string = 'hello'
+}
