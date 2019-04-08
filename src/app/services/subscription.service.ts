c4ff33abc026ee70c5a5120314506d923ab9fb5e
Alan Xie <alanxie29@gmail.com>
2019-04-08 16:37:39 -0400

    add subscription method front end

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -13,13 +13,36 @@ import { AlertController } from '@ionic/angular';
 export class SubscriptionService {
 
   url = environment.url;
+  userId: string = ''
   
   constructor(
     private http: HttpClient,
     private storage: Storage,
     private alertController: AlertController,
     ) { }
+
+  
+  createSubscription(data) { console.log(data)
+    this.getUserId()
+    return this.http.put(`${this.url}/api/create/${this.userId}`, data)
+    .pipe(
+      tap(res => {
+        this.storage.set('userData', res);
+      }),
+      catchError(e => {
+        this.showAlert(e.error.msg);
+        throw new Error(e);
+      })
+    );
+  };
  
+  getUserId() {
+    this.storage.get('userData').then((val) => {
+      this.userId = val.user._id
+      console.log(this.userId);
+    });
+  };
+
   showAlert(msg) {
     let alert = this.alertController.create({
       message: msg,
